de24422ff64a1e164da0ea15f484a27c1e186b10
Hogeyama <gan13027830@gmail.com>
2022-04-11 22:55:05 +0900

    My new gear...

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -5,21 +5,24 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "uas" "sd_mod" "sdhci_pci" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "nvme" "uas" "sdhci_pci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/ca28aaaf-e5da-427b-b9e0-8931049f109a";
+    {
+      device = "/dev/disk/by-uuid/192a4b3e-dd0f-4690-a241-b18c821df301";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/B95B-A7FC";
+    {
+      device = "/dev/disk/by-uuid/3D7C-0794";
       fsType = "vfat";
     };
 
