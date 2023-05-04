7b37a84c057d53b002b50fafa7862505349e5bde
Hogeyama <gan13027830@gmail.com>
2023-05-05 00:16:05 +0900

    fix(nix): Add hydra.iohk.io to binary caches

--- configuration.nix
+++ configuration.nix
@@ -18,12 +18,13 @@ in
     package = pkgs.unstable.nixVersions.nix_2_15;
     settings = {
       substituters = [
-        "s3://hogeyama-nix-cache?region=ap-northeast-1"
         "https://cache.iog.io"
+        "https://hydra.iohk.io"
+        "s3://hogeyama-nix-cache?region=ap-northeast-1"
       ];
       trusted-public-keys = [
-        "hogeyama-nix-cache:23HHz6x8J47bSCM0z6kZ++3x1ZXVPorsv3AJg1yqwAQ="
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+        "hogeyama-nix-cache:23HHz6x8J47bSCM0z6kZ++3x1ZXVPorsv3AJg1yqwAQ="
       ];
       auto-optimise-store = true;
     };
