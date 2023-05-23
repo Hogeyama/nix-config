488ff7ffced46fb22e812a8524ea03a6b7b2bbb7
Hogeyama <gan13027830@gmail.com>
2023-05-23 21:58:37 +0900

    feat(corretto): Update corretto-17 hash

--- pkgs/amazon-corretto17/default.nix
+++ pkgs/amazon-corretto17/default.nix
@@ -1,10 +1,10 @@
 { pkgs }:
-pkgs.stdenv.mkDerivation rec {
+pkgs.stdenv.mkDerivation {
   name = "amazon-corretto-17";
   src = pkgs.fetchzip {
     # https://docs.aws.amazon.com/corretto/latest/corretto-17-ug/downloads-list.html から
     url = "https://corretto.aws/downloads/latest/amazon-corretto-17-x64-linux-jdk.tar.gz";
-    sha256 = "sha256-jqwTurLWnLPMWjopJ2IZ6M7aUbsUhNFZ5sd9jFDkJoA=";
+    sha256 = "sha256-DEkfpGiqcas4HcAc327uMZj5BDR29JYSP0g4sEbFVSU=";
   };
   nativeBuildInputs = [
     pkgs.autoPatchelfHook
