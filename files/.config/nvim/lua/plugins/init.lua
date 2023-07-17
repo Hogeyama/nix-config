cb643c7a7bbcbb40df4f5326177205355ee6e892
Hogeyama <gan13027830@gmail.com>
2023-07-18 00:07:02 +0900

    tmp(nvim): Disable treesitter's highlight
    なんかバグっとる

--- files/.config/nvim/lua/plugins/init.lua
+++ files/.config/nvim/lua/plugins/init.lua
@@ -159,7 +159,7 @@ return {
         sync_install = false,
         auto_install = false,
         highlight = {
-          enable = true,
+          enable = false,
           disable = function(_, buf)
             local max_filesize = 100 * 1024 -- 100 KB
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
