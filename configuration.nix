de89db0d5454db7896dd9413398ef39847b3686e
Hogeyama <gan13027830@gmail.com>
2024-03-27 15:52:58 +0900

    nix: Update nix to 2.19

--- configuration.nix
+++ configuration.nix
@@ -15,7 +15,7 @@ in
     ];
 
   nix = {
-    package = pkgs.unstable.nixVersions.nix_2_17;
+    package = pkgs.unstable.nixVersions.nix_2_19;
     settings = {
       substituters = [
         "https://cache.iog.io"
