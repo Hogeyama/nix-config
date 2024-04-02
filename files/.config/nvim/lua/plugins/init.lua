5784ebbf9cb019f07a6911cfbad016c5c4de1b63
Hogeyama <gan13027830@gmail.com>
2024-04-02 15:14:12 +0900

    nvim: Let diffview be a toplevel dependency

--- files/.config/nvim/lua/plugins/init.lua
+++ files/.config/nvim/lua/plugins/init.lua
@@ -1780,28 +1780,28 @@ _K_: prev hunk   _u_: undo stage hunk   _p_: preview hunk   _B_: blame show full
     end,
     dependencies = {
       { 'iceberg.vim' },
-      {
-        'sindrets/diffview.nvim',
-        init = function()
-          local actions = require("diffview.actions")
-          require("diffview").setup {
-            view = {
-              merge_tool = {
-                layout = "diff4_mixed",
-                disable_diagnostics = true,
-                winbar_info = true,
-              },
-            },
-            keymaps = {
-              file_panel = {
-                { "n", "<CR>", actions.goto_file_edit },
-                { 'n', 'q',    function() vim.cmd("tabclose") end, { desc = "Close" } },
-              },
-            },
-          }
-        end,
-      },
     },
+  },
+  {
+    'sindrets/diffview.nvim',
+    init = function()
+      local actions = require("diffview.actions")
+      require("diffview").setup {
+        view = {
+          merge_tool = {
+            layout = "diff4_mixed",
+            disable_diagnostics = true,
+            winbar_info = true,
+          },
+        },
+        keymaps = {
+          file_panel = {
+            { "n", "<CR>", actions.goto_file_edit },
+            { 'n', 'q',    function() vim.cmd("tabclose") end, { desc = "Close" } },
+          },
+        },
+      }
+    end,
   },
   {
     'tpope/vim-fugitive',
