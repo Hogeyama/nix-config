5ccc91d93fa34963ec036c5daefd3e6ff1b9bb26
hogeyama <gan13027830@gmail.com>
2024-04-28 00:06:20 +0900

    nix: Decouple fonts configuration

--- modules/configuration/default.nix
+++ modules/configuration/default.nix
@@ -190,37 +190,6 @@
     };
   };
 
-  fonts = {
-    packages = [
-      pkgs.udev-gothic.nerdfont
-      pkgs.udev-gothic.jpdoc
-      pkgs.rounded-mgenplus
-      (pkgs.nerdfonts.override { fonts = [ "Hack" ]; })
-      pkgs.noto-fonts-emoji
-      pkgs.noto-fonts-cjk-sans
-      pkgs.noto-fonts-cjk-serif
-    ];
-    fontconfig = {
-      #ultimate.enable = true;
-      defaultFonts = {
-        monospace = [
-          "Illusion N"
-          "Rounded Mgen+ 1mn"
-          "Noto Color Emoji" # Emoji fallback
-          "Hack Nerd Font" # Nerd font fallback
-          "Noto Sans Mono CJK JP" # Other fallback
-        ];
-        sansSerif = [
-          "Rounded Mgen+ 1cp"
-          "Noto Sans CJK JP"
-        ];
-        serif = [
-          "Noto Serif CJK JP"
-        ];
-      };
-    };
-  };
-
   security.sudo.wheelNeedsPassword = false;
 
   services.keybase = {
