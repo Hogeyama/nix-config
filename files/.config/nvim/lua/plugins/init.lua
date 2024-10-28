358bca578e8a7c3b1e4a6aa440d740fbac61e94d
hogeyama <gan13027830@gmail.com>
2024-10-29 00:27:04 +0900

    nvim: Fix deprecation warning

--- files/.config/nvim/lua/plugins/init.lua
+++ files/.config/nvim/lua/plugins/init.lua
@@ -13,7 +13,7 @@ return {
       render = "compact",
       stages = "static",
       on_open = function(win)
-        vim.api.nvim_win_set_option(win, "winblend", 30)
+        vim.api.nvim_set_option_value("winblend", 30, { win = win })
         vim.api.nvim_win_set_config(win, { zindex = 100 })
       end
     },
