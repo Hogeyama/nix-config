681008d792fe9e9c34b7a84f3a3d90467527e198
Naoki Iwayama <naoki.iwayama@asahinet.com>
2024-11-05 00:05:25 +0900

    nvim: Disable jdtls on vscode

--- files/.config/nvim/ftplugin/java.lua
+++ files/.config/nvim/ftplugin/java.lua
@@ -6,6 +6,10 @@ vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
 
+if vim.g.vscode then
+  return
+end
+
 --------------------------------------------------------------------------------
 -- LSP/DAPサーバーのパス解決
 --------------------------------------------------------------------------------
