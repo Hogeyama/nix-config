4f1bacf8ca9833bd6b5ae6986e64c52e45e35433
Naoki Iwayama <naoki.iwayama@asahinet.com>
2025-01-31 22:18:07 +0900

    nvim: Remove vue-goto-definition.nvim
    なくても大丈夫な気がした

--- files/.config/nvim/lua/plugins/init.lua
+++ files/.config/nvim/lua/plugins/init.lua
@@ -2344,34 +2344,6 @@ return {
   },
   -- [[vue]]
   {
-    "catgoose/vue-goto-definition.nvim",
-    event = "BufReadPre",
-    enabled = not is_light_mode and not vim.g.vscode,
-    opts = {
-      filters = {
-        auto_imports = true,
-        auto_components = true,
-        import_same_file = true,
-        declaration = true,
-        duplicate_filename = true,
-      },
-      filetypes = { "vue", "typescript" },
-      detection = {
-        nuxt = function()
-          return vim.fn.glob(".nuxt/") ~= ""
-        end,
-        vue3 = function()
-          return vim.fn.filereadable("vite.config.ts") == 1 or vim.fn.filereadable("src/App.vue") == 1
-        end,
-        priority = { "nuxt", "vue3" },
-      },
-      lsp = {
-        override_definition = true, -- override vim.lsp.buf.definition
-      },
-      debounce = 200
-    },
-  },
-  {
     'posva/vim-vue',
     enabled = not is_light_mode and not vim.g.vscode,
   },
