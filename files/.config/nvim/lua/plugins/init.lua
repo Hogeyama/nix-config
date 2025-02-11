4dfe8a2add229f3d7c37e31ee8a3fa5e9d987742
hogeyama <gan13027830@gmail.com>
2025-02-11 20:40:43 +0900

    nvim: Try avante

--- files/.config/nvim/lua/plugins/init.lua
+++ files/.config/nvim/lua/plugins/init.lua
@@ -331,8 +331,60 @@ return {
     end,
   },
   {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false,
+    opts = {
+      provider = "copilot",
+      openai = {
+        endpoint = "https://api.openai.com/v1",
+        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+        timeout = 30000,  -- timeout in milliseconds
+        temperature = 0,  -- adjust if needed
+        max_tokens = 4096,
+      },
+    },
+    build = "make",
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "hrsh7th/nvim-cmp",            -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua",            -- for file_selector provider fzf
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua",      -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+  {
     "CopilotC-Nvim/CopilotChat.nvim",
-    enabled = true and not vim.g.vscode,
+    enabled = false and not vim.g.vscode,
     event = "UIEnter",
     dependencies = {
       { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
