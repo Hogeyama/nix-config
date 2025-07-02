1bd51503669b49830aca7869f6ed75cf2c5ce6e9
cq2n-iwym <naoki.iwayama@asahi-net.com>
2025-07-02 17:55:43 +0900

    nvim: プラグイン更新

--- files/.config/nvim/lua/plugins/init.lua
+++ files/.config/nvim/lua/plugins/init.lua
@@ -307,49 +307,6 @@ return {
     }
   },
   {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false,
-    opts = {
-      provider = "copilot",
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-        timeout = 30000,  -- timeout in milliseconds
-        temperature = 0,  -- adjust if needed
-        max_tokens = 4096,
-      },
-    },
-    build = "make",
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "ibhagwan/fzf-lua",            -- for file_selector provider fzf
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",      -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-    },
-  },
-  {
     'nvim-treesitter/nvim-treesitter',
     event = "VeryLazy",
     enabled = true and not vim.g.vscode,
@@ -1124,6 +1081,7 @@ return {
   {
     'saghen/blink.cmp',
     enabled = not vim.g.vscode,
+    version = 'v1.4.1',
     build = 'nix run .#build-plugin',
     opts = {
       keymap = {
@@ -1948,16 +1906,18 @@ return {
         dependencies = {
           {
             'williamboman/mason-lspconfig.nvim',
+            version = 'v1.11.0',
             config = true,
           },
           {
             'williamboman/mason.nvim',
+            version = 'v1.11.0',
             opts = {
-              registries = {
-                -- 'github:Hogeyama/jdtls-mason-registry',
-                'github:nvim-java/mason-registry',
-                'github:mason-org/mason-registry',
-              },
+              -- registries = {
+              --   -- 'github:Hogeyama/jdtls-mason-registry',
+              --   'github:nvim-java/mason-registry',
+              --   'github:mason-org/mason-registry',
+              -- },
             }
           },
         },
