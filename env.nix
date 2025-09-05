66043bec394f57ad038b571925a90a6b4d614224
hogeyama <gan13027830@gmail.com>
2025-09-06 00:55:20 +0900

    env: fcitx5を自動で起動する
    なんか勝手に落ちてることがあるので

--- env.nix
+++ env.nix
@@ -125,5 +125,29 @@ rec {
       package = null;
       portalPackage = null;
     };
+
+    # Hyprlandがsuspendから復帰するときにfcitx5と接続が切れてfcitx5がexit 0するらしい。
+    # > Sep 06 13:13:53 nixos uwsm_Hyprland[3375]: error in client communication (pid 101812)
+    # > Sep 06 13:13:53 nixos fcitx5[101812]: wl_display#1: error 0: invalid object 17
+    # > Sep 06 13:13:53 nixos fcitx5[101812]: E2025-09-06 13:13:53.873618 waylandeventreader.cpp:129] Wayland connection got error: 22
+    # Restart=alwaysで毎回再起動するようにしておく。
+    systemd.user.services.fcitx5 = {
+      Unit = {
+        Description = "Fcitx5 input method daemon";
+        After = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+      Service = {
+        # わざわざ pkgs.fcitx5-with-addons.override ... を modules/locale/default.nix から公開して……
+        # とかやるほどのことでもないので直パスを指定する。どうせ Debian 環境では apt で入れた fcitx5 を使うのだし。
+        ExecStart = "/run/current-system/sw/bin/fcitx5 -r";
+        Restart = "always";
+        RestartSec = 2;
+      };
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+      };
+    };
+
   };
 }
