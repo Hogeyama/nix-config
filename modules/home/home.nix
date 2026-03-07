21293f11e02e335c9fc1fd869601d1c7e4735758
hogeyama <gan13027830@gmail.com>
2026-03-07 14:06:18 +0900

    zsh: Add pnpm bin path

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -567,6 +567,7 @@ in
         }
         test -z "''${ZSHENV_LOADED}" || return
         export ZSHENV_LOADED=1
+        export PATH="$HOME/.local/share/pnpm:$PATH"
         export PATH="$HOME/.local/bin:$PATH" # prefer .local/bin
         export FZF_DEFAULT_COMMAND='fd --hidden --follow --exclude .git'
         export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
